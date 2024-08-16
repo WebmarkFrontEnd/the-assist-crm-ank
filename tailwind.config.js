714118684cde56adc61b63b10c6cccd7b59bf1d1
Harendra Singh <harendra@teamwebmark.com>
2024-08-16 10:49:48 +0530

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      'xs': { 'max': '480px' },
+      'sm': { 'min': '481px', 'max': '767px' },
+      'md': { 'min': '768px', 'max': '979px' },
+      'lg': { 'min': '980px', 'max': '1024px' },
+      'xl': { 'min': '1025px', 'max': '1180px' },
+      '2xl': { 'min': '1181px', 'max': '1439px' },
+      '3xl': { 'min': '1440px' },
+    },
+    container: {
+      center: true,
+      screens: {
+        xs: '302px',
+        sm: '462px',
+        md: '608px',
+        lg: '90%',
+        xl: '90%',
+        '2xl': '1396px',
+      },
+    },
 
+    colors: {
+      'primary': '#7B9645',
+      'secondary': '#53514D',
+      'light': '#fff',
+    },
+
+    plugins: [],
+  }
+}
